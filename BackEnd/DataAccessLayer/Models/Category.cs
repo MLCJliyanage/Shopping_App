c1194b16fec9264dbadda4e283a994a6e09794b9
chathura070 <chathura070@gmail.com>
2020-06-11 11:38:34 +0530

    First Commit

---
+++ BackEnd/DataAccessLayer/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataAccessLayer.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Category_id { get; set; }
+        [Required]
+        public string Category_name { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(256)")]
+        public string Image { get; set; }
+        [Column(TypeName ="text")]
+        public string Description { get; set; }
+        [Required]
+        public DateTime Date_added { get; set; }
+        public DateTime Date_modified { get; set; }
+
+    }
+}
---
+++ BackEnd/ShoppingApp_WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingApp_Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            yield return "Welcome to Shoppling App Backend";
+        }
+    }
+}
